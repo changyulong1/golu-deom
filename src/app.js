030c29696f5173832758ba8aa2813d8c485dfa75
long <853044263@qq.com>
2022-05-08 22:58:54 +0800

    使用 chai.expect 添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -13,3 +13,89 @@ new Vue({
         loading3:false
     }
 })
+
+
+
+
+
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setup'
+        }
+    })
+    vm.$mount()
+    const useElement = vm.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-setup')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setup',
+            loading:true
+        }
+    })
+    vm.$mount()
+    const useElement = vm.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setup',
+        }
+    })
+    vm.$mount(div)
+    const svg = vm.$el.querySelector('svg')
+    const {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setup'
+        }
+    })
+    vm.$mount(div)
+    const svg = vm.$el.querySelector('svg')
+    const {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setup',
+            iconPosition:'right'
+        }
+    })
+    vm.$mount(div)
+    vm.$on('click',()=>{
+        console.log()
+    })
+
+}
