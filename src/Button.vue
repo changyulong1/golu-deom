3e60ce4f1f96e9e4f0fffef2cadcade471b90ebd
long <853044263@qq.com>
2022-05-08 18:04:57 +0800

    填写 icon 和 iconPosition 属性

--- src/Button.vue
+++ src/Button.vue
@@ -1,9 +1,17 @@
 <template>
-  <button class="g-button">hai</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg class="icon" v-if="iconName">
+      <use :xlink:href="`#i-${iconName}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
+  props:['iconName','iconPosition']
 }
 </script>
 
@@ -12,9 +20,18 @@ export default {
   height: var( --button-height);padding: 0 1em;font-size: var( --font-size);
   border-radius: var(--border-radius);border: 1px solid var(--border-color);
   background: var( --button-bg);
+  display: inline-flex;
+  justify-content: center;align-items: center;
+  vertical-align: middle;
   &:hover{border-color: var(--border-color-hover);}
   &:active{background-color: var(--button-active-bg);}
   &:focus{outline: none}
+  >.icon{order:1;margin-right:.3em;}
+  >.content{order: 2}
+  &.icon-right{
+    >.icon{order: 2;margin-right: 0;margin-left: .3em;}
+    >.content{order: 1}
+  }
 }
 
 
