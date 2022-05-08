a2ca99d20fa7b6bd0b5e512c9b929eb725c944dd
long <853044263@qq.com>
2022-05-08 23:50:49 +0800

    电源测试代码优化

--- src/app.js
+++ src/app.js
@@ -24,81 +24,97 @@ import chai from 'chai'
 import spies from "chai-spies"
 chai.use(spies)
 const expect = chai.expect
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setup'
-        }
-    })
-    vm.$mount()
-    const useElement = vm.$el.querySelector('use')
-    const href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-setup')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setup',
-            loading:true
-        }
-    })
-    vm.$mount()
-    const useElement = vm.$el.querySelector('use')
-    const href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setup',
-        }
-    })
-    vm.$mount(div)
-    const svg = vm.$el.querySelector('svg')
-    const {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setup',
-            iconPosition:'right'
-        }
-    })
-    vm.$mount(div)
-    const svg = vm.$el.querySelector('svg')
-    const {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setup',
 
-        }
+try {
+    //测试button按钮是否有icon
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setup'
+            }
+        })
+        vm.$mount()
+        const useElement = vm.$el.querySelector('use')
+        const href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-setup')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+//测试是否开始加载icon
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setup',
+                loading:true
+            }
+        })
+        vm.$mount()
+        const useElement = vm.$el.querySelector('use')
+        const href = useElement.getAttribute('xlink:href')
+        expect(href).to.eq('#i-loading')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+//测试默认的icon的默认位置
+    {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setup',
+            }
+        })
+        vm.$mount(div)
+        const svg = vm.$el.querySelector('svg')
+        const {order} = window.getComputedStyle(svg)
+        expect(order).to.eq('1')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+//测试添加iconPosition后icon的位置
+    {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setup',
+                iconPosition:'right'
+            }
+        })
+        vm.$mount(div)
+        const svg = vm.$el.querySelector('svg')
+        const {order} = window.getComputedStyle(svg)
+        expect(order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+//测试button的click是否执行
+
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setup',
+
+            }
+        })
+        vm.$mount()
+        //模拟事件执行函数
+        let spy = chai.spy(function (){})
+        vm.$on('click',spy)
+        const button = vm.$el
+        button.click()
+        expect(spy).to.have.been.called()
+    }
+
+}catch (error){
+    window.errors = [error]
+}finally {
+    window.errors && window.errors.forEach((error) => {
+        console.error(error.message)
     })
-    vm.$mount()
-    //模拟事件执行函数
-    let spy = chai.spy(function (){})
-    vm.$on('click',spy)
-    const button = vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
 }
