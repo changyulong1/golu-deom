2f9dca8db0bd32fc0b368dddbea743b295f79305
long <853044263@qq.com>
2022-05-22 03:12:26 +0800

    使用VuePress搭建官网

--- src/Button.vue
+++ src/Button.vue
@@ -33,19 +33,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$button-height: 32px;
+$font-size: 14px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
 @keyframes spin {
   0% { transform: rotate(0deg)}
   100% { transform: rotate(360deg)}
 }
 .g-button{
-  height: var( --button-height);padding: 0 1em;font-size: var( --font-size);
-  border-radius: var(--border-radius);border: 1px solid var(--border-color);
-  background: var( --button-bg);
+  height: $button-height;padding: 0 1em;font-size: $font-size;
+  border-radius: $border-radius;border: 1px solid $border-color;
+  background: $button-bg;
   display: inline-flex;
   justify-content: center;align-items: center;
   vertical-align: middle;
-  &:hover{border-color: var(--border-color-hover);}
-  &:active{background-color: var(--button-active-bg);}
+  &:hover{border-color: $border-color-hover;}
+  &:active{background-color: $button-active-bg;}
   &:focus{outline: none}
   >.icon{order:1;margin-right:.3em;}
   >.content{order: 2}
